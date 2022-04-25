5b04b5f65801ac70c5a78581f0d781c7604a1623
bengris32 <bengris32@protonmail.ch>
2022-04-25 13:25:00 +0100

    utils: Code cleanup

--- oplus_unlock/utils/bootloader.py
+++ oplus_unlock/utils/bootloader.py
@@ -1,25 +1,22 @@
 import io
 import struct
 
-sequences = [
-    bytes.fromhex("2de9f04fadf5ac5d"), # Android 9
-    bytes.fromhex("f0b5adf5925d")      # Android 10/11
-]
-
 class BootloaderImage:
-    magic    = None
-    size     = None
-    name     = None
-    loaddr   = None
-    mode     = None
-    sequence = None
-    offset   = None
+    SEQUENCES = [
+        bytes.fromhex("2de9f04fadf5ac5d"), # Android 9
+        bytes.fromhex("f0b5adf5925d")      # Android 10/11
+    ]
 
-
-    def __init__(self, image, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, image: str):
         self.image = image
-    
+        self.magic = None
+        self.size = None
+        self.name = None
+        self.loaddr = None
+        self.mode = None
+        self.sequence = None
+        self.offset = None
+
     def parse_image(self):
         with open(self.image, 'r+b') as f:
             f.seek(0x4040 if f.read(4) == b'BFBF' else 0)
@@ -27,14 +24,14 @@ class BootloaderImage:
 
             self.magic, self.size, self.name, self.loaddr, \
                 self.mode = header.unpack(f.read(header.size))
-            
+
             if int(hex(self.magic), 0) != 0x58881688:
                 raise Exception("invalid magic")
-            
+
             if int(hex(self.loaddr), 0) == 0xffffffff:
                 while header != b'\x10\xff\x2f\xe1':
                     header = f.read(4)
-                
+
                 f.seek(f.tell() + 4)
                 self.loaddr = struct.unpack("<I", f.read(4))[0]
 
@@ -42,15 +39,15 @@ class BootloaderImage:
         with open(self.image, 'r+b') as f:
             data = f.read()
 
-        for seq in sequences:
+        for seq in self.SEQUENCES:
             offset = data.find(seq)
             if offset != -1:
                 self.sequence, self.offset = seq, offset
 
-        if self.sequence == None:
+        if self.sequence is None:
             raise Exception("no suitable sequence was found")
 
-    def update_lock_sequence(self, out):
+    def update_lock_sequence(self, out: str):
          with open(out, 'w+b') as o:
              with open(self.image, 'r+b') as f:
                  # 00 20 => movs r0, #0x0 
--- oplus_unlock/utils/logger.py
+++ oplus_unlock/utils/logger.py
@@ -1,23 +1,14 @@
-import datetime
-
-LOGGING_LEVELS = {
-    "0": "[*]", # Unknown (Default)
-    "1": "[?]", # Info
-    "2": "[!]", # Warning
-    "3": "[-]", # Error
-    "4": "[+]", # Success
-}
-
 class Logger:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    LOGGING_LEVELS = {
+        0: "[*]", # Unknown (Default)
+        1: "[?]", # Info
+        2: "[!]", # Warning
+        3: "[-]", # Error
+        4: "[+]", # Success
+    }
 
     def log(self, buf, prio = 0):
-        if (prio := str(prio)) in LOGGING_LEVELS:
-            line = f"{LOGGING_LEVELS[prio]}: " + buf
-        else:
-            line = LOGGING_LEVELS["0"] + buf
-
+        line = f"{self.LOGGING_LEVELS.get(prio, self.LOGGING_LEVELS[0])}: {buf}"
         print(line)
 
     def die(self, msg, ecl):
